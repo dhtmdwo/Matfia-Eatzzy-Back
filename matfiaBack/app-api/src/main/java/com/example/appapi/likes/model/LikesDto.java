43797e1dfd00c7e33d39173ae0a75bf296469be9
seungjae <dhtmdwo73@naver.com>
2025-02-18 20:20:40 +0900

    [feat] 좋아요 목록 조회(#24)
    좋아요 목록 조회
    
    Resolves : #24

--- matfiaBack/app-api/src/main/java/com/example/appapi/likes/model/LikesDto.java
+++ matfiaBack/app-api/src/main/java/com/example/appapi/likes/model/LikesDto.java
@@ -1,14 +1,36 @@
 package com.example.appapi.likes.model;
 
+import com.example.appapi.likes.model.Likes;
+import com.example.appapi.likes.model.LikesDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 public class LikesDto {
     @Getter
     public static class CourseRegister {
+        private String message;
         public Likes toEntity() {
             return Likes.builder()
+                    .message(message)
                     .build();
         }
     }
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class LikesResponse {
+        private String message;
+
+        public static LikesDto.LikesResponse from(Likes likes) {
+            return LikesDto.LikesResponse.builder()
+                    .message(likes.getMessage())
+                    .build();
+        }
+
+    }
+    
 }
