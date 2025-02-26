9bc50cdd82adc5abd682b4d151fd892fda95e672
seungho99 <shsm66@naver.com>
2025-02-26 18:14:04 +0900

    [Feature] StoreReview 엔티티 생성
    StoreReview 엔티티 생성

---
+++ app-api/src/main/java/com/example/appapi/store/review/StoreReviewController.java
@@ -0,0 +1,4 @@
+package com.example.appapi.store.review;
+
+public class StoreReviewController {
+}
---
+++ app-api/src/main/java/com/example/appapi/store/review/model/StoreReviewDto.java
@@ -0,0 +1,4 @@
+package com.example.appapi.store.review.model;
+
+public class StoreReviewDto {
+}
