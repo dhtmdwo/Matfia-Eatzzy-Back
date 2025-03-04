51103e4d8ec150274395100ecf8341f45185f1b1
taehoon0518 <cjsxogns0518@gmail.com>
2025-03-04 21:38:01 +0900

    [Feat] 주문 상세보기 주문서 상세보기 기능 추가

--- app-api/src/main/java/com/example/appapi/orders/OrdersRepository.java
+++ app-api/src/main/java/com/example/appapi/orders/OrdersRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
     Orders findByIdx(Long idx);
@@ -15,6 +16,13 @@ public interface OrdersRepository extends JpaRepository<Orders, Long> {
             "JOIN FETCH o.orderProducts op " +
             "JOIN FETCH op.products p")
     List<Orders> findAllWithOrderProductsAndProducts();
+
+    @Query("SELECT DISTINCT o FROM Orders o " +
+            "JOIN FETCH o.orderProducts op " +
+            "JOIN FETCH op.products p " +
+            "WHERE o.idx = :ordersIdx")
+    Optional<Orders> findByIdWithOrderProductsAndProducts(@Param("ordersIdx") Long ordersIdx);
+
 
     @Modifying
     @Query("UPDATE Orders o SET o.price = :totalPrice WHERE o.idx = :orderIdx")
