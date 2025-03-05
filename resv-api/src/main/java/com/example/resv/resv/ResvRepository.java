3ea7f58cfec1da850ddb82daef2056c6bc3519e7
seiyeon <wsx4379@naver.com>
2025-03-05 16:34:43 +0900

    [Feat] 식당 예약 생성 기능 구현
    식당 예약 생성 기능 구현

---
+++ resv-api/src/main/java/com/example/resv/resv/ResvRepository.java
@@ -0,0 +1,7 @@
+package com.example.resv.resv;
+
+import com.example.resv.resv.model.Resv;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResvRepository extends JpaRepository<Resv, Long> {
+}
