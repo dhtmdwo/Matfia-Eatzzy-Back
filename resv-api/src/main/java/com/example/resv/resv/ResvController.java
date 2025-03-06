4b14d3ad382f4c0460c3961deded69b62743c3ed
seiyeon <wsx4379@naver.com>
2025-03-06 12:54:08 +0900

    [Fix] 예약 기능 수정 (BaseResponse, Auth 인증 추가)
    예약 기능 수정 (BaseResponse, Auth 인증 추가)

--- resv-api/src/main/java/com/example/resv/resv/ResvController.java
+++ resv-api/src/main/java/com/example/resv/resv/ResvController.java
@@ -1,6 +1,8 @@
 package com.example.resv.resv;
 
 import com.example.appapi.users.model.Users;
+import com.example.common.BaseResponse;
+import com.example.common.BaseResponseStatus;
 import com.example.resv.resv.model.ResvDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +18,27 @@ import java.util.List;
 public class ResvController {
     private final ResvService resvService;
 
+    
+    @Operation(summary = "식당 예약하기")
     @PostMapping("/create")
-    public ResponseEntity<ResvDto.ResvResponse> create(@AuthenticationPrincipal Users user, @RequestBody ResvDto.CreateResvRequest dto) {
+    public ResponseEntity<BaseResponse<ResvDto.ResvResponse>> create(@AuthenticationPrincipal Users user, @RequestBody ResvDto.CreateResvRequest dto) {
         ResvDto.ResvResponse resv = resvService.create(dto, user);
 
-        return ResponseEntity.ok(resv);
+        return ResponseEntity.ok(new BaseResponse(BaseResponseStatus.SUCCESS, resv));
     }
 
-    @Operation(summary = "예약한 식당 내역 보기(클라이언트)")
+    @Operation(summary = "예약한 식당 내역 보기 (클라이언트)")
     @GetMapping("/mypage/store")
-    public ResponseEntity<List<ResvDto.StoreRezResponse>> storeList(@RequestParam("idx") Long idx) {
-        List<ResvDto.StoreRezResponse> responseList = resvService.storeList(idx);
-        return ResponseEntity.ok(responseList);
+    public ResponseEntity<BaseResponse<List<ResvDto.StoreRezResponse>>> storeList(@AuthenticationPrincipal Users user) {
+        List<ResvDto.StoreRezResponse> responseList = resvService.storeList(user);
+        return ResponseEntity.ok(new BaseResponse(BaseResponseStatus.SUCCESS, responseList));
     } // 마이페이지 클라이언트 예약한 식당 내역 보기
 
     @Operation(summary = "예약 취소하기(클라이언트)")
     @GetMapping("/mypage/deletestore")
-    public ResponseEntity<String> deleteReservation(@RequestParam("idx") Long idx) {
+    public ResponseEntity<BaseResponse<String>> deleteReservation(@RequestParam("idx") Long idx) {
         resvService.deleteReservation(idx);
-        return ResponseEntity.ok("삭제 완료");
+        return ResponseEntity.ok(new BaseResponse(BaseResponseStatus.SUCCESS, "예약이 성공적으로 취소되었습니다."));
     } // 마이페이지 클라이언트 예약 취소
 
 }
